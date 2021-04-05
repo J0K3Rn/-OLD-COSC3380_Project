b6d2e9312856108ffdac0a0cafb3255e64ce027b
Egr711 <garcia1999eric@gmail.com>
2021-04-04 22:21:38 -0500

    Fixed some bugs in createanimal

--- zoo-website/src/services/service.js
+++ zoo-website/src/services/service.js
@@ -3,7 +3,6 @@ import axios from "axios"
 class DataService{
     //this sends "data" to the backend
     createAnimal(data){
-        alert("work")
         return axios
             .post("http://localhost:8080/api/CreateAnimal", {
                 data
